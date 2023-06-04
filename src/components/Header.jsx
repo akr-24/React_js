8c7cd6e24ab637d06281c4a5bc783f1699203ea9
Aman <amankumarg014@gmail.com>
2023-06-05 01:15:50 +0530

    few more changes

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const MyHeading=({name,value=200})=>{
+   // alert(props.value);
+    //alternatively we can use props as argument and then we can go for props.name and props.value to access the objects.
+    return <h1>hello ji kaise ho {name},{value}</h1>
+
+};
+
+
+
+export default MyHeading;
+
+
